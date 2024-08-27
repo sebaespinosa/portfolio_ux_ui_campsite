47fd023674f2a78513fd5830b1e0064eefc5c931
seba espinosa <sebaespinosa@gmail.com>
2024-08-27 09:15:48 -0400

    Features section lista

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -1,7 +1,71 @@
+import { FEATURES } from "@/constants"
+import Image from "next/image"
 
 const Features = () => {
   return (
-    <div>Features</div>
+    <section className="border-2 border-red-500 flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative w-full flex justify-end">
+        <div className="flex flex-1 lg:min-h-[900px]">
+          <Image 
+           src='/phone.png'
+           alt='phone'
+           width={440}
+           height={1000}
+           className="feature-phone" />
+        </div>
+
+        <div className="z-20 flex w-full flex-col lg:w-[60%]">
+          <div className="relative">
+            <Image
+              width={50}
+              height={50}
+              objectFit="cover"
+              src='/camp.svg'
+              alt="camp"
+              className="absolute left-[-5px] top-[-28px] w-10 lg:w-[50px]"
+            />
+            <h2 className="bold-40 lg:bold-64">Our Features</h2>
+            <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:gap-20">
+              {FEATURES.map( (feature) => (
+                <FeatureItem
+                 title={feature.title}
+                 key={feature.title}
+                 icon={feature.icon}
+                 description={feature.description} />
+              ) )}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+type FeatureItemProps = {
+  title: string;
+  icon: string;
+  description: string;
+}
+
+const FeatureItem = ({ title, icon, description }:FeatureItemProps) => {
+  return(
+    <li className="flex w-full flex-1 flex-col items-start">
+      <div className="rounded-full p-4 lg:p-7 bg-green-50">
+        <Image
+          width={28}
+          height={28}
+          objectFit="cover"
+          src={icon}
+          alt={title}
+        />
+      </div>
+      <h2 className="bold-20 mt-5 capitalize lg:bold">
+        {title}
+      </h2>
+      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
+        {description}
+      </p>
+    </li>
   )
 }
 
